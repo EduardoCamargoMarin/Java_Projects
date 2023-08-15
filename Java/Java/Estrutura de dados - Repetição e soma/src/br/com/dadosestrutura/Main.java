114b97e14c0014b4198b91a17495046a8fb72cd7
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-08-15 16:29:38 -0300

    Add files via upload

---
+++ Java/Java/Estrutura de dados - Repetição e soma/src/br/com/dadosestrutura/Main.java
@@ -0,0 +1,17 @@
+package br.com.dadosestrutura;
+
+public class Main {
+    public static void main(String[] args) {
+
+     int soma = 0;
+        for(int i = 0; i <= 100; i++) {
+            soma +=i;
+        }
+        System.out.println(soma);
+
+
+
+
+
+    }
+}
