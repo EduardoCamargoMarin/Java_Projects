090370de4ed2ad8812ccf47c40ec226ad4afdc58
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2024-03-12 10:14:33 -0300

    Add files via upload

---
+++ Java/Java/br.com.VeiculoMarcaModelo/src/Carro.java
@@ -0,0 +1,9 @@
+public class Carro extends Veiculo{
+    public Carro (String marca, String modelo, int ano) {
+        super(marca, modelo, ano);
+    }
+
+    public Carro() {
+        super();
+    }
+}
---
+++ Java/Java/br.com.VeiculoMarcaModelo/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Carro carroItem01 = new Carro("Pegeout","206",2004);
+        Carro carroItem02 = new Carro("Audi", "TT", 2016);
+        Carro carroItem03 = new Carro("Fiat", "Uno", 2020);
+
+        Moto motoItem01 = new Moto("Yamaha", "125 Yes", 2000);
+
+    }
+}
